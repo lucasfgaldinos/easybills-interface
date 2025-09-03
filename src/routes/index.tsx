c6884c6570ef8332180f51009c696598556c73a2
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-09-03 18:18:40 -0300

    chore: config inicial com React + Vite, biomejs e react router

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Home } from "../screens/home";
+
+export function AppRoutes() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<h2>Página não encontrada!</h2>} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
