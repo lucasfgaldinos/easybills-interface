9051002cf4daf99e1283887254299f87f96901b5
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-09-06 14:30:23 -0300

    feat: criar tela de Home

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -39,7 +39,7 @@ export function Button({
 	function renderLoading() {
 		return (
 			<div className="flex items-center justify-center">
-				<div className="w-4 h-4 border-2 border-t-white border-gray-800 rounded-4xl animate-spin mr-3" />
+				<div className="w-4 h-4 border-2 border-t-white border-gray-800 rounded-full animate-spin mr-3" />
 				{children}
 			</div>
 		);
