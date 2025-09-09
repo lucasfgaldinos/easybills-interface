0093bf24bbdb4022fed1dd35cbdfb37b69f6627a
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-09-09 16:09:58 -0300

    feat: config api com axios

--- src/screens/dashboard.screen.tsx
+++ src/screens/dashboard.screen.tsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import { api } from "../services/api";
+
 export function DashboardScreen() {
+	useEffect(() => {
+		async function getTransactions() {
+			try {
+				const response = await api.get("/transactions");
+
+				console.log(response);
+			} catch (_) {
+				console.error("Error making API request.");
+			}
+		}
+
+		getTransactions();
+	}, []);
+
 	return (
 		<div>
 			<h1>Tela de Dashboard</h1>
