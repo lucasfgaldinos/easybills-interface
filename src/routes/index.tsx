4b24fb55ed84368c238fb5aaf1ed01d79d9d7c8a
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-09-15 15:44:12 -0300

    chore: config toastify na aplicação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router";
+import { ToastContainer, type ToastContainerProps } from "react-toastify";
 import { AuthProvider } from "../context/auth.context";
 import { DashboardLayout } from "../layout/dashboard.layout";
 import {
@@ -10,6 +11,18 @@ import {
 import { PrivateRoutes } from "./private-routes.route";
 
 export function AppRoutes() {
+	const toastConfig: ToastContainerProps = {
+		position: "top-right",
+		autoClose: 3000,
+		hideProgressBar: false,
+		newestOnTop: true,
+		closeOnClick: true,
+		pauseOnHover: true,
+		pauseOnFocusLoss: true,
+		draggable: true,
+		theme: "colored",
+	};
+
 	return (
 		<BrowserRouter>
 			<AuthProvider>
@@ -26,6 +39,7 @@ export function AppRoutes() {
 
 					<Route path="*" element={<h2>Página não encontrada!</h2>} />
 				</Routes>
+				<ToastContainer {...toastConfig} />
 			</AuthProvider>
 		</BrowserRouter>
 	);
