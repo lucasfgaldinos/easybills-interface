8cd42a090cf027b17e7430388dcf59f9e840edb5
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-09-18 11:34:03 -0300

    feat: finalizar footer

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -1,7 +1,11 @@
 export function Footer() {
 	return (
-		<footer>
-			<h1>Meu Footer</h1>
+		<footer className="bg-bg-dark border-t mt-15 border-neutral-base py-4">
+			<div className="container-app">
+				<p className="text-center text-sm font-medium text-text-muted">
+					EasyBills 2025 - Desenvolvido por <strong>Lucas Galdino</strong>
+				</p>
+			</div>
 		</footer>
 	);
 }
